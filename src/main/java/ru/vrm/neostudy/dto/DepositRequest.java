e42622b07a47a1de018cf6eef310f23e094f1d5a
1vanshh <shamaev.ivan2005@gmail.com>
2026-09-18 13:44:25 +0300

    fix: imports for DepositRequest

--- src/main/java/ru/vrm/neostudy/dto/DepositRequest.java
+++ src/main/java/ru/vrm/neostudy/dto/DepositRequest.java
@@ -1,7 +1,10 @@
 package ru.vrm.neostudy.dto;
 
-import jakarta.validation.constraints.*;
-
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 /**
